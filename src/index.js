a5bb0eaa902f9879b7d97968950fef8053e68a86
AT <atxiesz@gmal.com>
2022-01-28 17:20:51 +0800

    初始

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home, User, Profile } from "./components"
 
 ReactDOM.render(
-  <div>hi</div>,
-  document.getElementById('root')
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home name="at" />} />
+      <Route path="/user" element={<User />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+)
+
+// 以下老的方式已废弃
+// Switch => Router
+// component => element
+// component = {home} => element
+// render => element
